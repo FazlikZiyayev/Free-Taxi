7d72308af7c9a50310e4501df2024228c4eafc05
fazlikziyaev <ziyayeva_f@mwi.uz>
2021-12-20 01:17:53 +0500

    Initial Commit

---
+++ Free Taxi/vc/ViewController.swift
@@ -0,0 +1,103 @@
+//
+//  ViewController.swift
+//  Free Taxi
+//
+//  Created by Fazlik Ziyaev on 17/12/21.
+//
+
+import UIKit
+import SideMenu
+import GoogleMaps
+import CoreLocation
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
+    
+    @IBOutlet weak var mapView: GMSMapView!
+    @IBOutlet weak var currentLocation: UILabel!
+    @IBOutlet weak var btnNext: UIButton!
+    @IBOutlet weak var adressBlock: UIView!
+    
+    var sideMenu: SideMenuNavigationController?
+    let locationManager = CLLocationManager()
+    let googleAPIKey = "AIzaSyDgLFdyaVq1I_cwfKb1V5F2N18_KWt1I50"
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        locationManager.delegate = self
+        sideMenu = SideMenuNavigationController(rootViewController: UIViewController())
+        SideMenuManager.default.leftMenuNavigationController = sideMenu
+        SideMenuManager.default.addPanGestureToPresent(toView: self.view)
+        adressBlock.layer.cornerRadius = adressBlock.frame.height / 6.0
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didFailWithError error: Error) {
+        print(error)
+    }
+    
+
+    
+    @IBAction func menuBynPressed(_ sender: UIButton) {
+        sideMenu?.leftSide = true
+        present(sideMenu! , animated: true)
+    }
+    
+    // Setting map to current location
+    @IBAction func locationBtnPressed(_ sender: UIButton) {
+        
+        mapView.camera = GMSCameraPosition(
+            target: CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0),
+            zoom: 13,
+            bearing: 0,
+            viewingAngle: 0)
+        
+        let marker = GMSMarker()
+        marker.position = CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0 )
+        marker.title = "Hey, hi"
+        marker.snippet = "Hey I am here!"
+        
+        marker.map = mapView
+        
+        let geocoder = GMSGeocoder()
+        geocoder.reverseGeocodeCoordinate(CLLocationCoordinate2D(latitude: locationManager.location?.coordinate.latitude ?? 0.0, longitude: locationManager.location?.coordinate.longitude ?? 0.0)) { response, error in
+            let address = response?.firstResult()
+            
+            if let safeDistrict = address?.subLocality,
+               let safeStreet = address?.thoroughfare {
+                
+                let correctedAddress = "\(safeDistrict), \(safeStreet)"
+                
+                DispatchQueue.main.async {
+                    self.currentLocation.text = correctedAddress
+                }
+            }
+        }
+        
+        
+        
+        // Authorize user
+        func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+            switch manager.authorizationStatus {
+            case .authorizedAlways:
+                return
+                
+            case .authorizedWhenInUse:
+                return
+                
+            case .denied:
+                return
+                
+            case .restricted:
+                locationManager.requestWhenInUseAuthorization()
+                
+            case .notDetermined:
+                locationManager.requestWhenInUseAuthorization()
+                
+            default:
+                locationManager.requestWhenInUseAuthorization()
+            }
+        }
+    }
+}
---
+++ Podfile
@@ -0,0 +1,11 @@
+platform :ios, '11.0'
+
+target 'Free Taxi' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Free Taxi
+  pod 'GoogleMaps'
+  pod 'GooglePlaces'
+  pod 'SideMenu'
+end
